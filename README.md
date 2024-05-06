350bf4272eccd4b6d3795e21c01dbd3cf94b6f51
Ziyeon <ziyeon.ivi@gmail.com>
2024-05-06 16:05:48 +0900

    🚀 Commit remaining files

---
+++ README.md
@@ -0,0 +1,88 @@
+### │ 소개
+
+서울도서관 웹사이트를 리디자인 후 반응형으로 제작했습니다
+
+Figma를 사용해 디자인 작업을 진행했고,
+
+Html 로 시멘틱한 구조를 잡은 후  javaScript로 동적인 기능을 추가했습니다
+
+<br />
+
+### │ Link
+
+**🔗 github**
+
+[https://github.com/ziynii/project__seoul-library-renewal-v2](https://github.com/ziynii/project__seoul-library-renewal-v2)
+
+🔗 **project site**
+
+[https://ziynii.github.io/project__seoul-library-renewal-v2/](https://ziynii.github.io/project__seoul-library-renewal-v2/)
+
+<br />
+
+### │ Skill
+
+🔸 **HTML**
+
+🔸 **SCSS**
+
+🔸 **JavaScript**
+
+<br />
+
+### │ Desktop
+
+🔸 **홈**
+
+<img src="https://user-images.githubusercontent.com/85431762/180263573-1f1acde2-0fcc-4282-a207-e2dfc56381f0.png" width="700px" />
+
+🔸 **자주 물어보는 질문**
+
+<img src="https://user-images.githubusercontent.com/85431762/180264589-5cb10d2a-2c4b-4ae5-b2ec-22b700801a5d.png" width="700px" />
+
+🔸 **공지사항**
+
+<img src="https://user-images.githubusercontent.com/85431762/180264500-b3a396e8-e25d-4b17-ad22-a831bbcc6bc6.png" width="700px" />
+
+<br />
+
+
+### │ Tablet
+
+🔸 **홈**
+
+<img src="https://user-images.githubusercontent.com/85431762/180264880-033b7e73-9a86-4275-aef2-726e21e586da.png" width="500px" />
+
+🔸 **자주 물어보는 질문**
+
+<img src="https://user-images.githubusercontent.com/85431762/180265107-cbf41db1-ed4a-41d0-8537-1c93b5cc1022.png" width="500px" />
+
+🔸 **공지사항**
+
+<img src="https://user-images.githubusercontent.com/85431762/180265044-e4e19904-73ef-497e-a811-e18ab61c8167.png" width="500px" />
+
+🔸 **메뉴**
+
+<img src="https://user-images.githubusercontent.com/85431762/180264958-24dc844c-0539-470e-aa7d-ffede4907563.png" width="500px" />
+
+<br />
+
+
+### │ Mobile
+
+🔸 **홈**
+
+<img src="https://user-images.githubusercontent.com/85431762/180265498-539f4123-b6bf-49d2-8af1-0fe997c52449.png" width="400px" />
+
+🔸 **자주 물어보는 질문**
+
+<img src="https://user-images.githubusercontent.com/85431762/180265369-d1af8c1e-9f51-4185-9932-c024700a707b.png" width="400px" />
+
+🔸 **공지사항**
+
+<img src="https://user-images.githubusercontent.com/85431762/180265449-20151125-66ab-4041-a06d-5a906878df66.png" width="400px" />
+
+🔸 **메뉴**
+
+<img src="https://user-images.githubusercontent.com/85431762/180265740-12947886-7cb2-482c-a4d7-dc317e35e2c7.png" width="400px" />
+
---
+++ scripts/build.js
@@ -0,0 +1,137 @@
+const path = require('path')
+const fs = require('fs')
+const { copySync } = require('fs-extra')
+
+const postcss = require('postcss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
+const imageminJpegtran = require('imagemin-jpegtran')
+
+const REPOSITORY_NAME = 'project__seoul-library-renewal-v2'
+const buildDir = path.resolve(__dirname, '../build')
+
+const faviconFileList = [
+  'android-chrome-192x192.png',
+  'android-chrome-512x512.png',
+  'apple-touch-icon.png',
+  'browserconfig.xml',
+  'favicon-16x16.png',
+  'favicon-32x32.png',
+  'favicon.ico',
+  'mstile-150x150.png',
+  'safari-pinned-tab.svg',
+  'site.webmanifest',
+]
+
+const faviconUrlList = [
+  '../assets/favicon/apple-touch-icon.png',
+  '../assets/favicon/favicon-32x32.png',
+  '../assets/favicon/favicon-16x16.png',
+  '../assets/favicon/site.webmanifest',
+  '../assets/favicon/safari-pinned-tab.svg',
+]
+
+function throwError(err) {
+  if (err) {
+    console.log('💥 Oops! Something went wrong')
+  }
+}
+
+function buildHtml() {
+  path.resolve(__dirname, '../index.html'), path.join(buildDir, 'index.html')
+
+  // Resolve pathname
+  fs.readFile(path.resolve(__dirname, '../index.html'), (err, data) => {
+    if (err) throwError(err)
+
+    let html = data.toString()
+    html = html.replaceAll('href="/"', `href="/${REPOSITORY_NAME}"`)
+
+    faviconUrlList.forEach((favicon) => {
+      if (!!REPOSITORY_NAME) {
+        const newFaviconUrl = favicon.replace('/', `/${REPOSITORY_NAME}/`)
+        html = html.replace(favicon, newFaviconUrl)
+      }
+    })
+
+    fs.writeFile(path.join(buildDir, 'index.html'), html, (err) => {
+      throwError(err)
+    })
+  })
+}
+
+function buildCss() {
+  const cssPath = path.join(__dirname, '../style.css')
+
+  fs.readFile(cssPath, (err, css) => {
+    if (err) {
+      throwError(err)
+      return
+    }
+
+    postcss([autoprefixer, cssnano])
+      .process(css, { from: cssPath })
+      .then((result) => {
+        fs.writeFile(path.join(buildDir, 'style.css'), result.css, throwError)
+      })
+  })
+}
+
+function copyFavicons() {
+  faviconFileList.forEach((filename) => {
+    copySync(
+      path.resolve(__dirname, `../assets/favicon/${filename}`),
+      path.join(buildDir, `assets/favicon/${filename}`)
+    )
+  })
+}
+
+async function optimizeImageAssets() {
+  const imagemin = (await import('imagemin')).default
+  try {
+    await imagemin(
+      [path.resolve(__dirname, '../assets/images/*.{jpg,png,svg}')],
+      {
+        destination: path.join(buildDir, 'assets/images'),
+        plugins: [imageminJpegtran()],
+      }
+    )
+  } catch (err) {
+    throwError(err)
+  }
+}
+
+async function build() {
+  console.log('------------------------')
+  console.log('Start building...')
+
+  // Create `build` directory
+  if (!fs.existsSync(buildDir)) fs.mkdirSync(buildDir)
+
+  // Build index.html & style.css
+  buildHtml()
+  buildCss()
+
+  // Copy js/*.js, assets/fonts/*, favicon files
+  copySync(
+    path.resolve(__dirname, '../index.html'),
+    path.join(buildDir, 'index.html')
+  )
+  copySync(
+    path.resolve(__dirname, '../subpage'),
+    path.join(buildDir, 'subpage')
+  )
+  copySync(path.resolve(__dirname, '../js'), path.join(buildDir, 'js'))
+  copySync(
+    path.resolve(__dirname, '../assets/fonts'),
+    path.join(buildDir, 'assets/fonts')
+  )
+  copyFavicons()
+
+  await optimizeImageAssets()
+
+  console.log('🎉 Successfully build your project')
+  console.log('🔜 Ready to deploy')
+}
+
+build()
